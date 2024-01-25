63e7fda877826426e418520eb0d0096c9b41e74e
EricaMarzi <erica.marzi.1993@gmail.com>
2024-01-25 16:04:12 +0100

    send message wip

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const app = createApp({
         return {
             user: data.user,
             contacts: data.contacts,
-            currentId: 1
+            currentId: 1,
+            newMessage: ''
         }
     },
     computed: {
@@ -14,10 +15,29 @@ const app = createApp({
         }
     },
     methods: {
-        
+        sendNewMessage(){
+            const nuovoMessaggio = {
+                id: 12,
+                date: '10/01/2020 15:30:55',
+                text: this.newMessage,
+                status: 'sent'
+              } 
+              this.contacts.messages.push(nuovoMessaggio)
+        }
     }
 });
 
 app.mount('#root')
 
 
+
+
+/*
+        const nuovoMessaggio = {
+          id: 0734097, => da generare
+          date: '10/01/2020 15:30:55', => prendere dinamicamente
+          text: this.newMessage
+          status: 'sent'
+        } 
+        this.contacts.messages.push(nuovoMessaggio)=> pushato dentro messages che è dentro currentContacts
+*/
